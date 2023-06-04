1c9e01a4c1ff0f9c2395e01aec2fe261001e3947
mordechay <m8572998@gmail.com>
2023-06-04 22:49:24 +0300

    break

--- app/src/main/java/com/mordechay/mefateach/selectversion/CentralizedActivity.java
+++ app/src/main/java/com/mordechay/mefateach/selectversion/CentralizedActivity.java
@@ -7,11 +7,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.Toast;
 
 import com.google.android.material.switchmaterial.SwitchMaterial;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CentralizedActivity extends AppCompatActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
 
+    private Map<String, SwitchMaterial> swiMap;
     private SwitchMaterial[] swiArray;
 
     @Override
@@ -21,29 +26,29 @@ public class CentralizedActivity extends AppCompatActivity implements View.OnCli
 
         Bundle extras = getIntent().getExtras();
 
-        swiArray = new SwitchMaterial[]{
-                findViewById(R.id.centralized_switch_internet),
+        swiMap = new HashMap<>();
+        swiMap.put("internet", (SwitchMaterial) findViewById(R.id.centralized_switch_internet));
+        swiMap.put("google_play_services", (SwitchMaterial) findViewById(R.id.centralized_switch_google_play_services));
+        swiMap.put("google_play", (SwitchMaterial) findViewById(R.id.centralized_switch_google_play));
+        swiMap.put("kosher_play", (SwitchMaterial) findViewById(R.id.centralized_switch_kosher_play));
+        swiMap.put("install_packages", (SwitchMaterial) findViewById(R.id.centralized_switch_install_packages));
+        swiMap.put("connect_to_computer", (SwitchMaterial) findViewById(R.id.centralized_switch_connect_to_computer));
+        swiMap.put("system_app_player", (SwitchMaterial) findViewById(R.id.centralized_switch_system_app_player));
+        swiMap.put("system_app_video", (SwitchMaterial) findViewById(R.id.centralized_switch_system_app_video));
+        swiMap.put("system_app_camera", (SwitchMaterial) findViewById(R.id.centralized_switch_system_app_camera));
+        swiMap.put("system_app_gallery", (SwitchMaterial) findViewById(R.id.centralized_switch_system_app_gallery));
+        swiMap.put("system_app_file_explorer", (SwitchMaterial) findViewById(R.id.centralized_switch_system_app_file_explorer));
+        swiMap.put("system_app_messages", (SwitchMaterial) findViewById(R.id.centralized_switch_system_app_messages));
+        swiMap.put("system_app_remote", (SwitchMaterial) findViewById(R.id.centralized_switch_system_app_remote));
 
-                findViewById(R.id.centralized_switch_google_play_services),
-                findViewById(R.id.centralized_switch_google_play),
-                findViewById(R.id.centralized_switch_kosher_play),
-
-                findViewById(R.id.centralized_switch_install_packages),
-                findViewById(R.id.centralized_switch_connect_to_computer),
-
-                findViewById(R.id.centralized_switch_system_app_player),
-                findViewById(R.id.centralized_switch_system_app_video),
-                findViewById(R.id.centralized_switch_system_app_camera),
-                findViewById(R.id.centralized_switch_system_app_gallery),
-                findViewById(R.id.centralized_switch_system_app_file_explorer),
-                findViewById(R.id.centralized_switch_system_app_messages),
-                findViewById(R.id.centralized_switch_system_app_remote)
-        };
-        for (SwitchMaterial switchMaterial : swiArray) {
-            switchMaterial.setOnCheckedChangeListener(this);
+        swiArray = new SwitchMaterial[swiMap.values().size()];
+        swiMap.values().toArray(swiArray);
+        for (Map.Entry<String, SwitchMaterial> switchMaterial : swiMap.entrySet()) {
+            switchMaterial.getValue().setOnCheckedChangeListener(this);
         }
 
-        int[] valueArr = extras.getIntArray("valueArray");
+        HashMap<String, Integer> valueMap = (HashMap<String, Integer>) getIntent().getSerializableExtra("valueArray");
+        int[] valueArr = valueMap.values();
 
         for (int i = 0; i < swiArray.length; i++) {
             if (valueArr[i] == Constants.INDEFINITE || valueArr[i] == Constants.SWITCH_OFF){
@@ -85,6 +90,35 @@ public class CentralizedActivity extends AppCompatActivity implements View.OnCli
 
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-
+        if (buttonView == swiMap.get("internet")){
+            {
+                SwitchMaterial switchMaterial = swiMap.get("kosher_play");
+                if (switchMaterial != null) {
+                    switchMaterial.setChecked(isChecked);
+                    switchMaterial.setEnabled(!isChecked);
+                }
+            }
+            {
+                SwitchMaterial switchMaterial = swiMap.get("google_play_services");
+                if (switchMaterial != null) {
+                    switchMaterial.setChecked(isChecked);
+                    switchMaterial.setEnabled(!isChecked);
+                }
+            }
+            {
+                SwitchMaterial switchMaterial = swiMap.get("google_play");
+                if (switchMaterial != null) {
+                    switchMaterial.setChecked(isChecked);
+                    switchMaterial.setEnabled(!isChecked);
+                }
+            }
+            {
+                SwitchMaterial switchMaterial = swiMap.get("system_app_remote");
+                if (switchMaterial != null) {
+                    switchMaterial.setChecked(isChecked);
+                    switchMaterial.setEnabled(!isChecked);
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/mordechay/mefateach/selectversion/Constants.java
+++ app/src/main/java/com/mordechay/mefateach/selectversion/Constants.java
@@ -1,5 +1,8 @@
 package com.mordechay.mefateach.selectversion;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Constants {
 
     public static final int INDEFINITE = 0;
@@ -9,12 +12,69 @@ public class Constants {
     public static final int SWITCH_DISABLE_ON = 4;
     public static final int SWITCH_LATER = 5;
 
-    public static final int[][] VALUE = new int[][]{
-            {Constants.INDEFINITE, Constants.INDEFINITE, Constants.INDEFINITE, Constants.INDEFINITE, Constants.INDEFINITE, Constants.INDEFINITE/* disable */, Constants.INDEFINITE, Constants.INDEFINITE, Constants.INDEFINITE, Constants.INDEFINITE, Constants.INDEFINITE, Constants.INDEFINITE/* disable */, Constants.INDEFINITE}, //default
-            {Constants.SWITCH_ON, Constants.SWITCH_ON, Constants.SWITCH_ON, Constants.SWITCH_ON, Constants.SWITCH_ON, Constants.SWITCH_OFF/* disable */, Constants.SWITCH_OFF, Constants.SWITCH_ON, Constants.SWITCH_ON, Constants.SWITCH_ON, Constants.SWITCH_ON, Constants.SWITCH_ON/* disable */, Constants.SWITCH_ON},
-            {Constants.SWITCH_ON, Constants.SWITCH_ON, Constants.SWITCH_ON, Constants.SWITCH_ON, Constants.SWITCH_ON, Constants.SWITCH_OFF/* disable */, Constants.SWITCH_OFF, Constants.SWITCH_OFF, Constants.SWITCH_OFF, Constants.SWITCH_OFF, Constants.SWITCH_OFF, Constants.SWITCH_ON/* disable */, Constants.SWITCH_ON},
-            {Constants.SWITCH_ON, Constants.SWITCH_ON, Constants.SWITCH_ON, Constants.SWITCH_ON, Constants.SWITCH_ON, Constants.SWITCH_OFF/* disable */, Constants.SWITCH_OFF, Constants.SWITCH_OFF, Constants.SWITCH_OFF, Constants.SWITCH_OFF, Constants.SWITCH_OFF, Constants.SWITCH_OFF/* disable */, Constants.SWITCH_ON}
+    public static final HashMap[] VALUE = new HashMap[]{
+            new HashMap<String, Integer>() {{
+                put("internet", Constants.INDEFINITE);
+                put("google_play_services", Constants.INDEFINITE);
+                put("google_play", Constants.INDEFINITE);
+                put("kosher_play", Constants.INDEFINITE);
+                put("install_packages", Constants.INDEFINITE);
+                put("connect_to_computer", Constants.INDEFINITE); /* disable */
+                put("system_app_player", Constants.INDEFINITE);
+                put("system_app_video", Constants.INDEFINITE);
+                put("system_app_camera", Constants.INDEFINITE);
+                put("system_app_gallery", Constants.INDEFINITE);
+                put("system_app_file_explorer", Constants.INDEFINITE);
+                put("system_app_messages", Constants.INDEFINITE); /* disable */
+                put("system_app_remote", Constants.INDEFINITE);
+            }},
+            new HashMap<String, Integer>() {{
+                put("internet", Constants.SWITCH_ON);
+                put("google_play_services", Constants.SWITCH_DISABLE_ON);
+                put("google_play", Constants.SWITCH_DISABLE_ON);
+                put("kosher_play", Constants.SWITCH_DISABLE_ON);
+                put("install_packages", Constants.SWITCH_ON);
+                put("connect_to_computer", Constants.SWITCH_OFF); /* disable */
+                put("system_app_player", Constants.SWITCH_OFF);
+                put("system_app_video", Constants.SWITCH_ON);
+                put("system_app_camera", Constants.SWITCH_ON);
+                put("system_app_gallery", Constants.SWITCH_ON);
+                put("system_app_file_explorer", Constants.SWITCH_ON);
+                put("system_app_messages", Constants.SWITCH_ON); /* disable */
+                put("system_app_remote", Constants.SWITCH_ON);
+            }},
+            new HashMap<String, Integer>() {{
+                put("internet", Constants.SWITCH_ON);
+                put("google_play_services", Constants.SWITCH_ON);
+                put("google_play", Constants.SWITCH_ON);
+                put("kosher_play", Constants.SWITCH_ON);
+                put("install_packages", Constants.SWITCH_ON);
+                put("connect_to_computer", Constants.SWITCH_OFF); /* disable */
+                put("system_app_player", Constants.SWITCH_OFF);
+                put("system_app_video", Constants.SWITCH_OFF);
+                put("system_app_camera", Constants.SWITCH_OFF);
+                put("system_app_gallery", Constants.SWITCH_OFF);
+                put("system_app_file_explorer", Constants.SWITCH_OFF);
+                put("system_app_messages", Constants.SWITCH_ON); /* disable */
+                put("system_app_remote", Constants.SWITCH_ON);
+            }},
+            new HashMap<String, Integer>() {{
+                put("internet", Constants.SWITCH_ON);
+                put("google_play_services", Constants.SWITCH_ON);
+                put("google_play", Constants.SWITCH_ON);
+                put("kosher_play", Constants.SWITCH_ON);
+                put("install_packages", Constants.SWITCH_ON);
+                put("connect_to_computer", Constants.SWITCH_OFF); /* disable */
+                put("system_app_player", Constants.SWITCH_OFF);
+                put("system_app_video", Constants.SWITCH_OFF);
+                put("system_app_camera", Constants.SWITCH_OFF);
+                put("system_app_gallery", Constants.SWITCH_OFF);
+                put("system_app_file_explorer", Constants.SWITCH_OFF);
+                put("system_app_messages", Constants.SWITCH_OFF); /* disable */
+                put("system_app_remote", Constants.SWITCH_ON);
+            }}
     };
+
 
 
     public static final int[] IMAGE = new int[]{R.drawable.ic_launcher_browser, R.drawable.ic_launcher_kosher_play, R.drawable.ic_launcher_app, R.drawable.ic_launcher_app, R.drawable.ic_launcher_app, R.drawable.ic_launcher_app, R.drawable.ic_launcher_app, R.drawable.ic_launcher_browser, R.drawable.ic_launcher_browser, R.drawable.ic_launcher_browser, R.drawable.ic_launcher_browser, R.drawable.ic_launcher_browser, R.drawable.ic_launcher_browser, R.drawable.ic_launcher_browser, R.drawable.ic_launcher_gallery3d};
